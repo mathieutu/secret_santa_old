68d63dc374d84683b511401c84f7e9ece5ea80ac
Mathieu TUDISCO <mathieu.tudisco@neoxia.com>
2016-11-22 17:10:18 +0100

    🔧 modify the user model

---
+++ app/Models/User.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    protected $fillable = [
+        'first_name', 'last_name', 'email',
+    ];
+}
