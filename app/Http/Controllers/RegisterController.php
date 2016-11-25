8e81f979ec7d1d145043851df72dd092e104de60
Mathieu TUDISCO <mathieu.tudisco@neoxia.com>
2016-11-25 11:53:04 +0100

    🐛 Fix Auth\AuthController -> RegisterController

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -1,8 +1,7 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use App\Models\User;
 use Laravel\Socialite\Contracts\Factory as Socialite;
 
