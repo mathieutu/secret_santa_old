06218a3764553be67c2aaa37b49378516c4f814d
Mathieu TUDISCO <mathieu.tudisco@neoxia.com>
2016-11-29 14:11:13 +0100

    🎨 increase images size and add credits

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,6 +2,7 @@
 <html lang="fr">
     <head>
         <link rel="stylesheet" type="text/css" href="resources/assets/sass/app.scss"/>
+        <link rel="stylesheet" type="text/css" href="resources/assets/sass/app.scss"/>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -28,5 +29,6 @@
                 <img src="{{asset('img/img-neoxia.png')}}" alt="by neoxia">
             </footer>
         </div>
+        <a class="credit" href="https://github.com/mathieutu/secret_santa">Powered with love by your UI/WebDev Team.</a>
     </body>
 </html>
